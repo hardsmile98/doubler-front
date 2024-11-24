d927ff27e57bb5ed790e495fb96ae1c42b703836
John Doe <johndoe@example.com>
2024-11-24 15:01:12 +0300

    f

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -9,3 +9,11 @@ export function setLS(key: string, value: string) {
 export function deleteLS(key: string) {
   window.localStorage.removeItem(key);
 }
+
+export function getRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + Math.ceil(min);
+}
+
+export function shuffle(array: Array<unknown>) {
+  return array.sort(() => Math.random() - 0.5);
+}
--- src/pages/Main/Game/index.tsx
+++ src/pages/Main/Game/index.tsx
@@ -1,5 +1,67 @@
+import prizeX0 from '@/assets/images/x0.webp';
+import prizeX2 from '@/assets/images/x2.webp';
+import { shuffle } from '@/helpers';
+import { useEffect, useRef, useState } from 'react';
+
+const prizes = [prizeX0, prizeX2];
+
+const totalDuplicates = 8;
+const defaultSize = 150;
+
 function Game() {
-  return <div>game</div>;
+  const slotRef = useRef<null | HTMLDivElement>(null);
+
+  const [items, setItems] = useState<string[]>(prizes);
+
+  const buildItemLists = (winItemIndex?: number) => {
+    const randomPrizes = shuffle(prizes.flatMap((i) => Array(totalDuplicates).fill(i))) as string[];
+
+    if (winItemIndex !== undefined) {
+      randomPrizes.push(prizes[winItemIndex]);
+    }
+
+    setItems(randomPrizes);
+  };
+
+  useEffect(() => {
+    buildItemLists();
+  }, []);
+
+  const startAnimation = () => {
+    if (slotRef.current) {
+      buildItemLists(0);
+
+      const totalHeight = prizes.length * totalDuplicates * defaultSize;
+
+      slotRef.current.animate(
+        [
+          {
+            transform: 'translateY(0)',
+          },
+          {
+            transform: `translateY(-${totalHeight}px)`,
+          },
+        ],
+        {
+          duration: 6000,
+          fill: 'forwards',
+          easing: 'ease-in-out',
+        },
+      );
+    }
+  };
+
+  return (
+    <div className='h-[100%] flex items-center justify-center'>
+      <div className='rounded-xl w-[150px] h-[150px] overflow-hidden'>
+        <div ref={slotRef}>
+          {items.map((prize, index) => (
+            <img key={index} className='block' src={prize} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
